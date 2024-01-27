b81a1396017e7d96bcfd203ef95172349b855b4f
nvdinh185 <nvdinh0766777123@gmail.com>
2024-01-27 16:48:36 +0700

    x

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ const App = () => {
     }
 
     const handleDelete = (student) => {
-        if (window.confirm('Bạn có chắc muốn xóa ?')) {
+        if (confirm('Bạn có chắc muốn xóa ?')) {
             // var newList = [...listStudents];
             // var idx = newList.findIndex(st => st.id == student.id);
             // newList.splice(idx, 1);
