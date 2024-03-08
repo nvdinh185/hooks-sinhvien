d9e0259323d6e612949bbbbe62000d6aa2b67a32
nvdinh185 <nvdinh0766777123@gmail.com>
2024-03-08 15:31:07 +0700

    x

--- src/App.js
+++ src/App.js
@@ -1,72 +1,40 @@
-import { useState } from 'react';
-
-let initialStudents = [
-    {
-        id: '1',
-        name: "Dinh",
-        address: "hue"
-    },
-    {
-        id: '2',
-        name: "Nam",
-        address: "quang nam"
-    },
-    {
-        id: '3',
-        name: "Tan",
-        address: "da nang"
-    },
-    {
-        id: '4',
-        name: "Hung",
-        address: "hue"
-    },
-    {
-        id: '5',
-        name: "Tri",
-        address: "quang tri"
-    },
-    {
-        id: '6',
-        name: "Anh",
-        address: "hue"
-    },
-    {
-        id: '7',
-        name: "Binh",
-        address: "da nang"
-    }
-]
-
 const App = () => {
-
-    const [listStudents] = useState(initialStudents);
 
     return (
         <>
             <form>
                 <div>
                     <label>Tên</label>
-                    <input type="text" name="name" />
+                    <input />
                 </div>
                 <br />
                 <div>
                     <label>Địa chỉ</label>
-                    <input type="text" name="address" />
+                    <input />
                 </div>
                 <div>
                     <button>Thêm</button>
                 </div>
             </form>
             <ul>
-                {listStudents.map((student, idx) =>
-                    <li key={idx}>
-                        <h2>Name: {student.name}</h2>
-                        <p>Address: {student.address}</p>
-                        <button>Sửa</button>
-                        <button>Xóa</button>
-                    </li>
-                )}
+                <li>
+                    <h2>Name: Định</h2>
+                    <p>Address: Huế</p>
+                    <button>Sửa</button>
+                    <button>Xóa</button>
+                </li>
+                <li>
+                    <h2>Name: Nam</h2>
+                    <p>Address: Quảng Nam</p>
+                    <button>Sửa</button>
+                    <button>Xóa</button>
+                </li>
+                <li>
+                    <h2>Name: Tân</h2>
+                    <p>Address: Đà Nẵng</p>
+                    <button>Sửa</button>
+                    <button>Xóa</button>
+                </li>
             </ul>
         </>
     )
